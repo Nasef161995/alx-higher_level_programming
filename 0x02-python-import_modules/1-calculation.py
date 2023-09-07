3126d39922459eaaf183cd591eb3619d63654c4f
Nasef161995 <mnasef905@gmail.com>
2023-09-07 05:08:08 -0700

    0x02. Python - import & modules

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -3,10 +3,10 @@ if __name__ == "__main__":
     from calculator_1 import add, sub, mul, div
     a = 10
     b = 5
-    addition  = add(a, b)
-    subtraction  = sub(a, b)
-    multiplication  = mul(a, b)
-    division  = div(a, b)
+    addition = add(a, b)
+    subtraction = sub(a, b)
+    multiplication = mul(a, b)
+    division = div(a, b)
     print("{:d} + {:d} = {:d}".format(a, b, addition))
     print("{:d} - {:d} = {:d}".format(a, b, subtraction))
     print("{:d} * {:d} = {:d}".format(a, b, multiplication))
