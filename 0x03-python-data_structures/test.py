3e1ad7af38d18108bf13a059e6b5bc6cd65c91d6
Nasef161995~ <mnasef905@gmail.com>
2023-09-09 13:41:11 +0300

    ccc

--- 0x03-python-data_structures/test.py
+++ 0x03-python-data_structures/test.py
@@ -11,3 +11,17 @@ for row in matrix:
         else:
             print(element, end=" ")
     print("$")
+
+
+
+
+
+
+def print_matrix_integer(matrix=[[]]):
+    for row in matrix:
+        for element in row:
+            if element == row[-1]:
+                print(element, end='')
+            else:
+                print(element, end=" ")
+        print("$")
