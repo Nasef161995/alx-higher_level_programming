71ce6e00de59e4233a958c3c0647c8c6b83b542e
Nasef161995~ <mnasef905@gmail.com>
2023-09-09 14:47:46 +0300

    vvvvv

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -6,4 +6,4 @@ def print_matrix_integer(matrix=[[]]):
                 print("{:d}".format(a), end='')
             else:
                 print("{:d}".format(a), end=' ')
-        print("$")
+        print()
