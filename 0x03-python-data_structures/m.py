c98747397f8f0c64e33b2499ef8d2fb03d163fb0
Nasef161995~ <mnasef905@gmail.com>
2023-09-09 20:52:58 +0300

    gggooooo

--- 0x03-python-data_structures/m.py
+++ 0x03-python-data_structures/m.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python3
-a = [1, 90, 2, 13, 34, 5, -13, 3]
-a.sort(reverse=True)
+my_list = [0, 1, 2, 3, 4, 5, 6]
 
-print(a[0])
+list_result = []
+for i in my_list:
+    if i % 2 == 0:
+        list_result.append(True)
+    else:
+        list_result.append(False)
+    
+print(list_result)
+print(len(list_result))
