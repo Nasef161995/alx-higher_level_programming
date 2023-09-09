6039e69e3ecb1d6c43c7d6542f1352aa1718038b
Nasef161995~ <mnasef905@gmail.com>
2023-09-10 00:06:58 +0300

    ......

--- 0x03-python-data_structures/m.py
+++ 0x03-python-data_structures/m.py
@@ -1,4 +1,33 @@
 #!/usr/bin/python3
-a = [0, 1, 2, 3, 4, 5, 6]
-a[6] = []
-print(a)
+def add_tuple(tuple_a=(), tuple_b=()):
+    if len(tuple_a) == 0:
+        a = tuple_a[0] = 0
+        b = tuple_a[1] = 0
+    elif len(tuple_b) == 0:
+        c = tuple_b[0] = 0
+        d = tuple_b[1] = 0
+    elif len(tuple_a) == 1:
+        b = tuple_a[1] = 0
+    elif len(tuple_b) == 1:
+        d = tuple_b[1] = 0
+    else:
+        a = tuple_a[0]
+        b = tuple_a[1]
+        c = tuple_b[0]
+        d = tuple_b[1]
+
+    return (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
+
+
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+   
+        
