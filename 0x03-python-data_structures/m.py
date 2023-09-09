73d5e14762bbe9ecccc3d02ae96b87aec2df1fa0
Nasef161995~ <mnasef905@gmail.com>
2023-09-09 21:09:30 +0300

    del

--- 0x03-python-data_structures/m.py
+++ 0x03-python-data_structures/m.py
@@ -1,12 +1,4 @@
 #!/usr/bin/python3
-my_list = [0, 1, 2, 3, 4, 5, 6]
-
-list_result = []
-for i in my_list:
-    if i % 2 == 0:
-        list_result.append(True)
-    else:
-        list_result.append(False)
-    
-print(list_result)
-print(len(list_result))
+a = [0, 1, 2, 3, 4, 5, 6]
+del a[6]
+print(a)
