b025d466db3c11bb987a3de2e3d0ecee58455cd6
Nasef161995~ <mnasef905@gmail.com>
2023-09-09 13:16:34 +0300

    666

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    if matrix == 0:
+        print("$")
+    else:
+        for a in range(len(matrix)):
+            for b in range(len(matrix)):
+                if b < len(matrix) - 1:
+                    print("{:d}".format(matrix[a][b]), end=' ')
+                elif b == len(matrix) - 1:
+                    print("{:d}".format(matrix[a][b]), end='')
+            else:
+                print("$")
--- 0x03-python-data_structures/test.py
+++ 0x03-python-data_structures/test.py
@@ -1,4 +1,11 @@
 #!/usr/bin/python3
-a = "mohamed"
-a[0]= 'h'
-print(a)
+matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+print(len(matrix))
+for i in range(3):
+    for j in range(3):
+        print(matrix[i][j], end=" ")
+    print()
