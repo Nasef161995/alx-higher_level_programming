fde54695354c3e83517d3050bb181b677fbf0518
Nasef161995~ <mnasef905@gmail.com>
2023-09-10 00:46:36 +0300

    finalllll

--- 0x03-python-data_structures/m.py
+++ 0x03-python-data_structures/m.py
@@ -1,33 +1,12 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    if len(tuple_a) == 0:
-        a = tuple_a[0] = 0
-        b = tuple_a[1] = 0
+    if len(tuple_a) >= 2 and len(tuple_b) >= 2:
+        return (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
+    elif len(tuple_a) == 0:
+        return (tuple_b[0], tuple_b[1])
     elif len(tuple_b) == 0:
-        c = tuple_b[0] = 0
-        d = tuple_b[1] = 0
-    elif len(tuple_a) == 1:
-        b = tuple_a[1] = 0
+        return (tuple_a[0], tuple_a[1])
     elif len(tuple_b) == 1:
-        d = tuple_b[1] = 0
-    else:
-        a = tuple_a[0]
-        b = tuple_a[1]
-        c = tuple_b[0]
-        d = tuple_b[1]
-
-    return (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
-
-
-
-    
-    
-    
-    
-    
-    
-    
-    
-    
-   
-        
+        return (tuple_a[0] + tuple_b[0], tuple_a[1])
+    elif len(tuple_a) == 1:
+        return (tuple_a[0] + tuple_b[0], tuple_b[1])
