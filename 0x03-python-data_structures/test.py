1330381c3bcc0b2af21ae23c188a407165f3e909
Nasef161995~ <mnasef905@gmail.com>
2023-09-10 17:13:39 +0300

    final

--- 0x03-python-data_structures/test.py
+++ 0x03-python-data_structures/test.py
@@ -1,38 +1,4 @@
 #!/usr/bin/python3
-matrix = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9]
-]
-for row in matrix:
-    for element in row:
-        if element == row[-1]:
-            print(element, end='')
-        else:
-            print(element, end=" ")
-    print("$")
-
-
-
-
-
-
-def print_matrix_integer(matrix=[[]]):
-    for row in matrix:
-        for element in row:
-            if element == row[-1]:
-                print(element, end='')
-            else:
-                print(element, end=" ")
-        print("$")
-
-
-def print_matrix_integer(matrix=[[]]):
-        for a in range(len(matrix)):
-            for b in range(len(matrix)):
-                if b < len(matrix) - 1:
-                    print("{:d}".format(matrix[a][b]), end=' ')
-                elif b == len(matrix) - 1:
-                    print("{:d}".format(matrix[a][b]), end='')
-            else:
-                print("$")
+a = 10, 
+a= a, 20
+print(a)
