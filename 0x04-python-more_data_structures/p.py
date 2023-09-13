42897b440c254b0481c48c64a02819dc64271c22
Nasef161995~ <mnasef905@gmail.com>
2023-09-13 18:58:45 +0300

    777

---
+++ 0x04-python-more_data_structures/p.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+user = {
+  "name": "Osama",
+  "age": 36,
+  "country": "Egypt",
+  "rating": 10.5
+}
+print(user)
+user.update({"language": "python"})
+print(user)
+user.update({"age": 50})
+print(user)
