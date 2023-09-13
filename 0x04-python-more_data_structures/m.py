f5a2bb773ad6499c605d7d35b098be8edd2e5d20
Nasef161995~ <mnasef905@gmail.com>
2023-09-14 00:53:08 +0300

    ffffffff

--- 0x04-python-more_data_structures/m.py
+++ 0x04-python-more_data_structures/m.py
@@ -1,17 +1,8 @@
 #!/usr/bin/python3
-def uniq_add(my_list=[]):
-    result = 0
-    for num in my_list:
-        if my_list.count(num) == 1: 
-            result = result + num
-        else:
-            i = 0
-            
-            if i == 0:
-                temp = num
-            if i > 0:
-                while i != my_list.count(num):
-                    my_list[my_list.index(num)] = 0
-                    i+=1
-            result = result + temp
-    return result
+a_dictionary = {'lang': "C", 'track': "Low", 'pref': "C", 'ids': [1, 2, 3]}
+a = a_dictionary.items()
+n = [(key, val) for key, val in a_dictionary.items() if val != "c"]
+
+
+
+print(n)
