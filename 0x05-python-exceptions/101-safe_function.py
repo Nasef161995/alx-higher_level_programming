42a92d9d88de5f1c52794699fb5604693c48d12c
Nasef161995~ <mnasef905@gmail.com>
2023-09-26 00:42:57 +0300

    finaaaal

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
+import sys
+
+
 def safe_function(fct, *args):
     try:
         result = fct
 
-
     except (TypeError, ValueError) as error:
-        print("Exception:", error)
-
-
+        print("Exception:", error, file=sys.stderr)
 
     return result
