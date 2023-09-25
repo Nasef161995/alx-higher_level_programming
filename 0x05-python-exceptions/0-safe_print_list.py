92695468a4e0dfa93d752fd0e13946a70b741284
Nasef161995~ <mnasef905@gmail.com>
2023-09-25 12:32:12 +0300

    task

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,10 +3,10 @@ def safe_print_list(my_list=[], x=0):
     i = 0
     for num in my_list:
         try:
-            if i <= x-1:
+            if i <= x - 1:
                 print(num, end='')
                 i += 1
-        except:
+        except BaseException:
             i = 1
             for n in my_list:
                 print(n, end='')
