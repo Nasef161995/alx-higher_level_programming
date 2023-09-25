7c43c1f27532562a9da52f17a5e66ae003718f6f
Nasef161995~ <mnasef905@gmail.com>
2023-09-25 22:57:14 +0300

    ffffff

--- 0x05-python-exceptions/a.py
+++ 0x05-python-exceptions/a.py
@@ -1,23 +1,9 @@
 #!/usr/bin/python3
-def safe_print_list_integers(my_list=[], x=0):
-    i = 0
-    c = 0
-    for a in my_list:
-        c+=1
-   
-    for num in my_list:
-        try:
-            if type(num) != int:
-                continue
-            if i <= x-1:
-                print("{:d}".format(num), end='')
-                i+=1 
-            if x > c:
-                print(my_list[x])
-           
-        except Exception as error:
+try:
+    i = -1
+    if i<0:
+        raise()
+    print("mmmm")
+except:
 
-            print(type(error).__name__)
-           
-    print()        
-    return i
+    print('ERROR')    
