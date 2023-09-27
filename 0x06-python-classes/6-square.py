c621be276017322ec671741c706c6240acca68eb
Nasef161995~ <mnasef905@gmail.com>
2023-09-28 01:27:03 +0300

    66666666

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -29,6 +29,10 @@ class Square:
     def position(self, value):
         if type(value) != tuple:
             raise Exception('position must be a tuple of 2 positive integers')
+        if not isinstance(value[0] and value[1], int):
+            raise Exception('position must be a tuple of 2 positive integers')
+        if value[0] < 0 and value[1] < 0:
+            raise Exception('position must be a tuple of 2 positive integers')
         self.__position = value
 
     def area(self):
