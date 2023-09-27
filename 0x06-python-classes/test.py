3ff25fd916005585c86f25073cc709cdf0f7e76b
Nasef161995~ <mnasef905@gmail.com>
2023-09-27 22:55:14 +0300

    4444

---
+++ 0x06-python-classes/test.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+class Book:
+    def __init__(self, name):
+        self.__name = name
+    def get_name():
+        return self.__name
+    def name(self, new):
+       self.get_name= new   
+m1 = Book("aaaaa")
+print(m1.name)
