85f22abff530214036ee2f0992d60023d6bc3dd4
Nasef161995~ <mnasef905@gmail.com>
2023-10-02 12:51:14 +0300

    task 00

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -14,7 +14,7 @@ class Rectangle:
     def width(self, value):
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
-        if value <0:
+        if value < 0:
             raise ValueError("width must be >= 0")
         self._Rectangle__width = value
 
@@ -25,6 +25,6 @@ class Rectangle:
     def height(self, value):
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
-        if value <0:
+        if value < 0:
             raise ValueError("height must be >= 0")
         self._Rectangle__height = value
--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -4,36 +4,40 @@
 
 class Rectangle:
     """Rectangle"""
+
     def __init__(self, width=0, height=0):
         self._Rectangle__width = width
         self._Rectangle__height = height
-    @property  
+
+    @property
     def width(self):
         return self._Rectangle__width
+
     @width.setter
     def width(self, value):
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
-        if value <0:
+        if value < 0:
             raise ValueError("width must be >= 0")
         self._Rectangle__width = value
 
-    @property  
+    @property
     def height(self):
         return self._Rectangle__height
+
     @height.setter
     def height(self, value):
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
-        if value <0:
+        if value < 0:
             raise ValueError("height must be >= 0")
         self._Rectangle__height = value
 
     def area(self):
         return self._Rectangle__height * self._Rectangle__width
-    
+
     def perimeter(self):
         if self._Rectangle__height == 0 or self._Rectangle__width == 0:
             return 0
         else:
-            return ( self._Rectangle__height + self._Rectangle__width) * 2
+            return (self._Rectangle__height + self._Rectangle__width) * 2
