040a75c242a211f885dfe5bd2c2f1d3a247d81a2
Nasef161995~ <mnasef905@gmail.com>
2023-10-04 11:32:04 +0300

    edit

--- 0x07-python-test_driven_development/2-main.py
+++ 0x07-python-test_driven_development/2-main.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 matrix_divided = __import__('2-matrix_divided').matrix_divided
 
-matrix = [[6, 9], [18, 15]]
+
     
    
 
-print(matrix_divided(matrix, -3))
+print(matrix_divided(3))
 print(matrix)
