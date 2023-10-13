e760495f871ccbcf29203b79e1be5e196262a223
Nasef161995~ <mnasef905@gmail.com>
2023-10-13 23:46:26 +0300

    333

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-"""Python script."""
-
-import json
+"""to_json_string"""
 
 
 def to_json_string(my_obj):
-    """JSON representation of an object (string)."""
-    return (json.dumps(my_obj))
+    """to_json_string"""
+    import json
+    new_json = json.dumps(my_obj)
+    return new_json
---
+++ 0x0B-python-input_output/tt.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+
+t = "This School is so cool!\n"
+print(len(t))
