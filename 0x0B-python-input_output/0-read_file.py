b169a855be774882970501d6b780f5f22073d8b4
Nasef161995~ <mnasef905@gmail.com>
2023-10-13 23:02:45 +0300

    00

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -4,5 +4,5 @@
 
 def read_file(filename=""):
     """read_file"""
-    file = open(filename, "r", encoding="utf-8")
-    print(file.read())
+    with open(filename, "r", encoding="utf-8") as file:
+        print(file.read())
