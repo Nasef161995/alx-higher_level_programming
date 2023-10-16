fe7792ddfe1a40f6224194a758fcb7a27b1a5ce7
Nasef161995~ <mnasef905@gmail.com>
2023-10-16 22:10:17 +0300

    000

--- 0x0C-python-almost_a_circle/m.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/python3
-m = [x, y , z]
-a = [1, 2, 3]
-m[0] = a[0]
-print(x)
--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Base"""
 import json
+import os
 
 
 class Base:
@@ -24,5 +25,39 @@ class Base:
     @classmethod
     def save_to_file(cls, list_objs):
         filname = f"{cls.__name__}.json"
+        new_list = []
+        if list_objs:
+            for i in list_objs:
+                new_list.append(cls.to_dictionary(i))
+        json_str = cls.to_json_string(new_list)
+
         with open(filname, "w", encoding='utf-8') as file:
-            json.dump(list_objs, file)
+            file.write(json_str)
+
+    @staticmethod
+    def from_json_string(json_string):
+        list = json.loads(json_string)
+        if json_string is None or json_string == "":
+            return []
+        else:
+            return list
+
+    @classmethod
+    def create(cls, **dictionary):
+        if cls.__name__ == "Rectangle":
+            dummy_istance = cls(1, 1)
+        else:
+            dummy_istance = cls(1)
+        dummy_istance.update(**dictionary)
+        return dummy_istance
+
+    @classmethod
+    def load_from_file(cls):
+        filename = f"{cls.__name__}.json"
+        new_list = []
+        if os.path.isfile(filename):
+            with open(filename, "r", encoding='utf-8') as file:
+                data = cls.from_json_string(file.read())
+            for i in data:
+                new_list.append(cls.create(**i))
+        return new_list
