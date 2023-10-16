53a290790505802dd0e06cf9525e7db9c568bbe8
Nasef161995~ <mnasef905@gmail.com>
2023-10-17 00:40:50 +0300

    000

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -1,18 +1,54 @@
-#!/usr/bin/python3
-"""TestBase class for test"""
 import unittest
+import sys
+from io import StringIO
 from models.base import Base
+from models.rectangle import Rectangle
 
 
 class TestBase(unittest.TestCase):
-    """TestBase class"""
+    """ Base Class """
 
-    def tets(self):
-        a = Base()
-        self.assertEqual(a.id, 1)
+    def test_id(self):
+        """ id """
 
-        a1 = Base()
-        self.assertEqual(a1.id, 2)
+        b1 = Base()
+        self.assertEqual(b1.id, 1)
 
-        a2 = Base(12)
-        self.assertEqual(a2.id, 12)
+        b2 = Base()
+        self.assertEqual(b2.id, 2)
+
+        b3 = Base(12)
+        self.assertEqual(b3.id, 12)
+
+        b4 = Base()
+        self.assertEqual(b4.id, 3)
+
+    def test_id_string(self):
+        """string"""
+
+        b1 = Base("string")
+        self.assertEqual(b1.id, "string")
+
+    def test_id_None(self):
+        """None"""
+
+        b1 = Base(None)
+        self.assertEqual(b1.id, 4)
+
+    def test_id_float(self):
+        """float"""
+
+        b1 = Base(1.2)
+        self.assertEqual(b1.id, 1.2)
+
+    def test_id_NaN(self):
+        """float nan"""
+
+        b1 = Base(float("nan"))
+        self.assertEqual(b1.id is float("nan"), False)
+
+    def test_id_inf(self):
+        """float inf"""
+
+        b1 = Base(float("inf"))
+        self.assertEqual(b1.id is float("inf"), False)
--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -1,30 +1,123 @@
-"""TestRectangle class for test"""
+#!/usr/bin/python3
+"""Unittest for Rectangle"""
+
+
 import unittest
+import sys
+from io import StringIO
+from models.base import Base
 from models.rectangle import Rectangle
 
 
 class TestRectangle(unittest.TestCase):
-    """TestRectangle class"""
+    """Rectangle class test"""
 
-    def tets(self):
-        a = Rectangle(10, 12)
-        self.assertEqual(a.id, 1)
+    def setUp(self):
+        """Resets nb_objects"""
+        Base._Base__nb_objects = 0
 
-        a1 = Rectangle(10, 12, 0, 0, 12)
-        self.assertEqual(a1.id, 12)
+    def test_id(self):
+        """Prints out the id"""
+        r1 = Rectangle(10, 2)
+        self.assertEqual(r1.id, 1)
 
-        with self.assertRaises(TypeError) as error:
-            Rectangle("a", 10)
-        self.assertEqual(str(error.exception), "width must be an integer")
+        r2 = Rectangle(2, 10)
+        self.assertEqual(r2.id, 2)
 
-        with self.assertRaises(ValueError) as error:
-            Rectangle(-1, 10)
-        self.assertEqual(str(error.exception), "width must be > 0")
+        r3 = Rectangle(10, 2, 0, 0, 12)
+        self.assertEqual(r3.id, 12)
+        self.assertTrue(type(r3), Rectangle)
 
-        with self.assertRaises(TypeError) as error:
-            x = Rectangle(9, 10)
-            x.width = "a"
-        self.assertEqual(str(error.exception), "width must be an integer")
+    def test_one_param(self):
+        """Passing one parameter"""
+        with self.assertRaises(TypeError):
+            r1 = Rectangle(1)
 
-        r3 = Rectangle(8, 7, 0, 0, 12)
-        self.assertEqual(r3.area(), 56)
+    def test_all_param(self):
+        """Passing all parameters"""
+        r1 = Rectangle(1, 1, 1, 1, 1)
+
+    def test_string(self):
+        """Passing string"""
+        with self.assertRaises(TypeError):
+            r1 = Rectangle(1, "string")
+        with self.assertRaises(TypeError):
+            r1 = Rectangle("string", 1)
+        with self.assertRaises(TypeError):
+            r1 = Rectangle(1, 2, 3, "4")
+
+    def test_no_param(self):
+        """Passing nothing"""
+        with self.assertRaises(TypeError):
+            r1 = Rectangle()
+
+    def test_excess_param(self):
+        """Excess parameters"""
+        with self.assertRaises(TypeError):
+            r1 = Rectangle(1, 1, 1, 1, 1, 1)
+
+    def test_float(self):
+        """Float parameter"""
+        with self.assertRaises(TypeError):
+            r1 = Rectangle(1, 1.2, 1, 1, 3)
+
+    def test_NaN(self):
+        """NaN parameter"""
+        with self.assertRaises(TypeError):
+            r1 = Rectangle(1, 2, float("nan"), 1, 1)
+
+    def test_inf(self):
+        """inf parameter"""
+        with self.assertRaises(TypeError):
+            r1 = Rectangle(1, 2, float("inf"), 1, 1)
+
+    def test_unknown(self):
+        """unknown parameter"""
+        with self.assertRaises(NameError):
+            r1 = Rectangle(a)
+
+    def test_None(self):
+        """None parameter"""
+        with self.assertRaises(TypeError):
+            r1 = Rectangle(1, 2, None, 1, 1)
+
+    def test_neg(self):
+        """Neg parameter"""
+        with self.assertRaises(ValueError):
+            r1 = Rectangle(-1, 2, 1, 1)
+        with self.assertRaises(ValueError):
+            r1 = Rectangle(1, -2)
+        with self.assertRaises(ValueError):
+            r1 = Rectangle(1, 2, 3, -4)
+
+    def test_area(self):
+        """Prints out area"""
+        r1 = Rectangle(10, 2)
+        self.assertEqual(r1.area(), 20)
+
+    def test_display(self):
+        """Tests rectangle output"""
+        output = StringIO()
+        sys.stdout = output
+        r1 = Rectangle(2, 2)
+        r1.display()
+        sys.stdout = sys.__stdout__
+        assert output.getvalue() == "##\n##\n"
+
+    def test_display_x_y(self):
+        """Tests rectangle output with x and y"""
+        output = StringIO()
+        sys.stdout = output
+        r2 = Rectangle(3, 2, 1, 0)
+        r2.display()
+        sys.stdout = sys.__stdout__
+        assert output.getvalue() == " ###\n ###\n"
+
+    def test_str(self):
+        """Tests __str__"""
+        output = StringIO()
+        sys.stdout = output
+        r1 = Rectangle(4, 6, 2, 1, 12)
+        print(r1)
+        sys.stdout = sys.__stdout__
+        assert output.getvalue() == "[Rectangle] (12) 2/1 - 4/6\n"
