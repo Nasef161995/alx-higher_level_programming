c318afa59420656b35a98e17f5862f45b7822f1c
Nasef161995~ <mnasef905@gmail.com>
2023-12-11 18:23:46 +0300

    222

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+if (process.arvgv[2] === 0)
+    console.log('No argument');
+else {
+    if (process.arvgv[3] === 0)
+        console.log('Argument found');
+    else
+        console.log('Arguments found');
+}
