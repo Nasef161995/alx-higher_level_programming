707710056c117c9a7d4dbf38a5bd064c99a9fb87
Nasef161995~ <mnasef905@gmail.com>
2023-12-11 19:30:53 +0300

    222

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,4 +1,10 @@
 #!/usr/bin/node
-if (process.argv[2] === 0) { console.log('No argument'); } else {
-  if (process.argv[3] === 0) { console.log('Argument found'); } else { console.log('Arguments found'); }
+if (!process.argv[2])
+{ console.log('No argument'); }
+else
+{
+    if (!process.argv[3])
+    { console.log('Argument found'); }
+    else
+    { console.log('Arguments found'); }
 }
