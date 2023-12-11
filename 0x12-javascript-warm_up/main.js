c63fd5d1eaf31888df1fe0dfa49c6b111aa1ce2c
Nasef161995~ <mnasef905@gmail.com>
2023-12-11 21:03:48 +0300

    333

--- 0x12-javascript-warm_up/main.js
+++ 0x12-javascript-warm_up/main.js
@@ -1,3 +1,6 @@
 #!/usr/bin/node
-const { argv } = require('node:process');
-console.log(length(argv));
+if (!process.argv[2]) {
+    console.log('No argument');
+}
+else
+    console.log(process.argv[2]);
