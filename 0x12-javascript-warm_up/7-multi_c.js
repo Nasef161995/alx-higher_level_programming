47f5419e79ad865814d1afb81ac5d15b68648472
Nasef161995~ <mnasef905@gmail.com>
2023-12-11 22:17:30 +0300

    7

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+if (Number.parseInt(process.argv[2])) {
+  const x = Number.parseInt(process.argv[2]);
+  for (let i = 0; i < x; i++) {
+    console.log('C is fun');
+  }
+} else {
+  console.log('Missing number of occurrences');
+}
