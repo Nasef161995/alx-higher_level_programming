634019a4a394e72edb583daf32cc9a5569de44fc
Nasef161995~ <mnasef905@gmail.com>
2023-12-12 23:37:56 +0300

    101

---
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+const dic = require('./101-data').dict;
+const newdict = {};
+for (const key in dic) {
+    if (newdict[dic[key]] === undefined) {
+        newdict[dic[key]] = [key];
+    } else {
+        newdict[dic[key]].push(key);
+    }
+}
+console.log(newdict);
