87097e94de60722e48d4f20347dba1da907e756c
Nasef161995~ <mnasef905@gmail.com>
2023-12-12 21:35:42 +0300

    666

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,1 +1,13 @@
 #!/usr/bin/node
+class Square extends require('./5-square') {
+  charPrint (c) {
+    for (let i = 0; i < this.height; i++) {
+      if (c === undefined) {
+        console.log('X'.repeat(this.height));
+      } else {
+        console.log(c.repeat(this.height));
+      }
+    }
+  }
+}
+module.exports = Square;
