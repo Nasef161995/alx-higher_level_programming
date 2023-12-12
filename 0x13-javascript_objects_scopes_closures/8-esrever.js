c8075d74a4689e8efc2e26d100fd428e6139b540
Nasef161995~ <mnasef905@gmail.com>
2023-12-12 22:10:40 +0300

    888

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,1 +1,10 @@
 #!/usr/bin/node
+exports.esrever = function (list) {
+  const newlist = [];
+  let element;
+  for (let i = list.length - 1; i >= 0; i--) {
+    element = list[i];
+    newlist.push(element);
+  }
+  return newlist;
+};
