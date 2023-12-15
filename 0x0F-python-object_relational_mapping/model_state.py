7c852400967dbbd68f742d9a2a42107e37b9d230
Nasef161995~ <mnasef905@gmail.com>
2023-12-15 19:00:42 +0300

    666

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+"""class state"""
+
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class State():
+    """class state"""
+
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String(128), nullable=False)
