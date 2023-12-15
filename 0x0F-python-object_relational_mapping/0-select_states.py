061475b2e51d7c9a60bd23e6fb6b4b3c50cd871e
Nasef161995~ <mnasef905@gmail.com>
2023-12-15 16:03:15 +0300

    000

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-import MySQLdb 
+import MySQLdb
 from sys import argv
 if __name__ == '__main__':
     db_connection = MySQLdb.connect(
         host='localhost',
-        user= argv[1],
-        password= argv[2],
-        database= argv[3],
+        user=argv[1],
+        password=argv[2],
+        database=argv[3],
         port=3306
     )
     cursor = db_connection.cursor()
