3e727a7bd52880d5676e0a8926fac7b33a56b85e
Nasef161995~ <mnasef905@gmail.com>
2023-12-16 11:48:58 +0300

    14

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,1 +1,27 @@
 #!/usr/bin/python3
+"""class city"""
+
+import sys
+from model_city import Base, City
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format
+                           (sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    result_cities = session.query(
+        State.name,
+        City.id,
+        City.name).order_by(
+        City.id). filter(
+            State.id == City.state_id).order_by(
+                City.id)
+
+    for element in result_cities:
+        print("{}: ({}) {}".format(element[0], element[1], element[2]))
+
+    session.close()
