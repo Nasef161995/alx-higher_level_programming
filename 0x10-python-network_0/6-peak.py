da2febe76e9fdf52295677205dbcf0ecf368b4ef
Nasef161995~ <mnasef905@gmail.com>
2024-01-05 23:30:11 +0300

    666

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+"""........."""
+
+def find_peak(list_of_integers):
+    """......."""
+    if not list_of_integers:
+        return (None)
+    peak = max(list_of_integers)
+    return (peak)
