a03eb074d18329f64cab9456ec5e28d3a478e85d
Nasef161995~ <mnasef905@gmail.com>
2024-01-05 23:13:09 +0300

    aaaa

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 #Bash script takes in a URL, sends a request to that URL
-curl -sI "$1" | grep -i "Allow" | awk -F ": " '{print $2}'
-
+curl -sI ALLOW $1 -L | grep "Allow" | cut -d " " -f2-
