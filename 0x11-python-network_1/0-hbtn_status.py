bd3db1b95b5de850af24a4b950ff5251eb2dca81
Nasef161995~ <mnasef905@gmail.com>
2024-01-06 20:33:21 +0300

    000

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -4,7 +4,7 @@
 if __name__ == "__main__":
     import urllib.request
 
-    with urllib.request.urlopen("https://alx-intranet.hbtn.io/status") as response:
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
         body = response.read()
         print("Body response:")
         print("\t- type: {}".format(type(body)))
