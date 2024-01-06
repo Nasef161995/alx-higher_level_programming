59faf0704fc7267394d524a6501d5a86af1dca05
Nasef161995~ <mnasef905@gmail.com>
2024-01-06 20:20:19 +0300

    000

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-from urllib.request import urlopen
-"""............."""
+"""Python script that fetches the webpage"""
 
 if __name__ == "__main__":
+    from urllib.request import urlopen
     with urlopen("https://alx-intranet.hbtn.io/status") as response:
         body = response.read()
     print("Body response:")
