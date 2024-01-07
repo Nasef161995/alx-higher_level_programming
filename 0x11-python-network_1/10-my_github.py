51c3a7c6f2cac2204c4263d1db544a99997a07dc
Nasef161995~ <mnasef905@gmail.com>
2024-01-07 23:18:53 +0300

    10

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+"""Python script that fetches webpage"""
+
+if __name__ == "__main__":
+    import requests
+    import sys
+    url = 'https://api.github.com/user'
+    username = sys.argv[1]
+    passwaord = sys.argv[2]
+    response = requests.post(url, auth=(username, passwaord))
+    try:
+        res = response.json()
+        id = res.get('id')
+        print(id)
+    except ValueError:
+        print("Not a valid JSON")
