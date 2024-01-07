f2d202370c94142640094fa330b28733fc0e3bd1
Nasef161995~ <mnasef905@gmail.com>
2024-01-07 21:27:29 +0300

    333

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+"""Python script that fetches webpage"""
+
+if __name__ == "__main__":
+    import urllib.request
+    import sys
+    from urllib.error import HTTPError
+    url = sys.argv[1]
+    try:
+        with urllib.request.urlopen(url) as r:
+            body = r.read().decode('utf-8')
+            print(body)
+    except HTTPError as error:
+        print("Error code:", error.code)
