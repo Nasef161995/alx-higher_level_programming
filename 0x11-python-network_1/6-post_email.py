32ea44a037ef451180e05ed631634e95d827d4e9
Nasef161995~ <mnasef905@gmail.com>
2024-01-07 21:54:44 +0300

    666

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+"""Python script that fetches webpage"""
+
+if __name__ == "__main__":
+    import requests
+    import sys
+    email = sys.argv[2]
+    url = sys.argv[1]
+    data = {"email": email}
+    response = requests.post(url, data)
+    print(response.text)
