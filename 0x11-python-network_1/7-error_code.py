9d3090112796174508bbb2f79812d71ebddfb289
Nasef161995~ <mnasef905@gmail.com>
2024-01-07 22:04:40 +0300

    7777

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+"""Python script that fetches webpage"""
+
+if __name__ == "__main__":
+    import requests
+    import sys
+    from urllib.error import HTTPError
+    url = sys.argv[1]
+    try:
+        response = requests.get(url)
+        print(response.text)
+    except HTTPError as error:
+        print("Error code:", error.code)
