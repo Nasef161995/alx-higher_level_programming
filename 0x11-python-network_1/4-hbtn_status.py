24595ec443618e638f81f482f4911c761b9489e1
Nasef161995~ <mnasef905@gmail.com>
2024-01-07 21:40:23 +0300

    444

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+"""Python script that fetches webpage"""
+
+if __name__ == "__main__":
+    import requests
+    response = requests.get("https://alx-intranet.hbtn.io/status")
+    print("Body response:")
+    print("\t- type: {}".format(type(response.text)))
+    print("\t- content: {}".format(response.text))
