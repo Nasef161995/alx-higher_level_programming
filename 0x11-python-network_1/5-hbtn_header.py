f2336a4e80282df0fd26fe2d0836cdf5f65eb7e7
Nasef161995~ <mnasef905@gmail.com>
2024-01-07 21:49:28 +0300

    55555

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+"""Python script that fetches webpage"""
+
+if __name__ == "__main__":
+    import requests
+    import sys
+    url = sys.argv[1]
+    response = requests.get(url)
+    headers = response.headers
+    id = headers.get("X-Request-Id")
+    print(id)
