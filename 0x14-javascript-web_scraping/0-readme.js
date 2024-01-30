4999fbc3d2485a09d331ceac88565adb8856e930
Nasef161995~ <mnasef905@gmail.com>
2024-01-30 22:34:16 +0300

    000

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -2,11 +2,10 @@
 const fs = require('fs');
 
 const pathfile = process.argv[2];
-fs.readFile(pathfile, 'utf-8', (error, data) => {
+fs.readFile(pathfile, 'utf8', (error, data) => {
   if (error) {
     console.error(error);
     return;
   }
-
   console.log(data);
 });
