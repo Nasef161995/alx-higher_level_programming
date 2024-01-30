9ff39ccbea20f1316153a48f133028bfff2785c7
Nasef161995~ <mnasef905@gmail.com>
2024-01-31 00:29:01 +0300

    4444

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,1 +1,16 @@
 #!/usr/bin/node
+
+const request = require('request');
+const url = process.argv[2];
+
+request(url, (err, res, body) => {
+  if (err) {
+    console.log(err);
+  }
+  const result = JSON.parse(body).results;
+  console.log(result.reduce((count, movie) => {
+    return movie.characters.find((character) => character.endsWith('/18/'))
+      ? count + 1
+      : count;
+  }, 0));
+});
