5e4638e7f084a356a307305ec066b3ca628f366e
Nasef161995~ <mnasef905@gmail.com>
2024-01-31 00:23:00 +0300

    333

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,1 +1,16 @@
 #!/usr/bin/node
+
+const request = require('request');
+const num = process.argv[2];
+const url = 'https://swapi-api.hbtn.io/api/films/';
+
+request(url + num, (err, res, body) => {
+  if (err) {
+    console.log(err);
+  } else if (res.statusCode === 200) {
+    const resJSON = JSON.parse(body);
+    console.log(resJSON.title);
+  } else {
+    console.log('Error code: ' + res.statusCode);
+  }
+});
