c7b744a9b89cbea9f50dfd12932ca5e0eb964680
Nasef161995~ <mnasef905@gmail.com>
2024-01-30 23:09:14 +0300

    111

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,1 +1,11 @@
 #!/usr/bin/node
+
+const fs = require('fs');
+const pathfile = process.argv[2];
+const content = process.argv[3];
+
+fs.writeFile(pathfile, content, 'utf8', (error) => {
+  if (error) {
+    console.log(error);
+  }
+});
